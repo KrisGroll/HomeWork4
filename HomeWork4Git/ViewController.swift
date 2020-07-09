40a3e4b23a9f2ae98eea5f97d8e753c4949d6303
KrisGroll <Kristina-gorelikova@mail.ru>
2020-07-09 14:22:46 +0200

    Initial Commit

---
+++ HomeWork4Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork4Git
+//
+//  Created by KrisGroll on 09.07.2020.
+//  Copyright © 2020 KrisGroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
