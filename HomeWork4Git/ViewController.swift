15da12d6f4c1e6a004dc5a21afd3b12a13533383
KrisGroll <Kristina-gorelikova@mail.ru>
2020-07-09 14:30:49 +0200

    HomeWork4

--- HomeWork4Git/ViewController.swift
+++ HomeWork4Git/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class Car {
+    var brand: String = ""
+    var color: String = ""
+    var volume: Double = 0
+    
+    func doAction() {
+        print("This is \(brand). The color of the car is \(color) and the car's volume is \(volume)." )
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class trunkCar: Car {
+    var carWeight: Double = 0
+    var trunkRoominess: Double = 0
+    
+    override func doAction() {
+        
+        print("The weight of the car is \(carWeight) kg and the roominess of the trunk is \(trunkRoominess) liters")
+        
+    }
+}
+
+class superCar: Car {
+    var speed: Double = 0
+    var model: String = ""
+    
+    override func doAction() {
+        
+        print("The max speed of the car is \(speed) km/h. That's why \(brand) \(model) is the fastest.")
+        }
     }
 
 
-}
+var c = Car()
+c.brand = "Bugatti"
+c.color = "Blue"
+c.volume = 1500
+c.doAction()
 
+var t = trunkCar()
+t.carWeight = 1888
+t.trunkRoominess = 60
+t.doAction()
+
+var s = superCar()
+s.speed = 431
+s.brand = "Bugatti"
+s.model = "Chiron"
+s.doAction()
+
+//этот код отлично работает в playground, но тут выдает ошибки. На консоли в плэйграунде код работает как надо:)
